fd92945f6fe8ad3937794e07f2a0d4a439f9bc84
Wei Guo <weiguouk@gmail.com>
2013-09-13 17:17:06 +0100

    WP-1121: Automate API dynamic build to defined profile

---
+++ libs/ant_config_profile.js
@@ -0,0 +1,31 @@
+/* Used by Ant build script task under project root directory */
+
+importClass(java.io.File);
+importClass(java.io.FileReader);
+importClass(java.io.BufferedReader);
+
+var br = new BufferedReader(new FileReader(basedir + "/config_profiles.json"));
+var json = "";
+var line = br.readLine();
+while(line != null) {
+    json += line;
+    line = br.readLine();
+}
+br.close();
+
+var target_profile = eval("(" + json + ")")[device];
+if(target_profile === undefined) {
+    var fail = project.createTask("fail");
+    fail.setMessage("Device profile " + device + " is undefined. Please use a valid profile in config_profiles.json.");
+    fail.perform();
+}
+
+for(var api in target_profile) {
+    var exec = project.createTask("exec");
+    exec.setDir(new File(basedir + "/node_modules/webinos-pzp"));
+    exec.setExecutable("npm");
+    exec.createArg().setValue("install");
+    exec.createArg().setValue(target_profile[api]);
+    exec.createArg().setValue("--save");
+    exec.perform();
+}
